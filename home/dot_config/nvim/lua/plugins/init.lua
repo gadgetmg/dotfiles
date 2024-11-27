1142bb25a430346a2af6bcc4dc636e125f52f76e
Matt Graham <gadgetmg@pm.me>
2024-11-27 12:31:49 -0500

    feat(nvim): Add LSPs and configure Mason

--- home/dot_config/nvim/lua/plugins/init.lua
+++ home/dot_config/nvim/lua/plugins/init.lua
@@ -12,6 +12,12 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      automatic_installation = true,
+    },
+  },
 
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
