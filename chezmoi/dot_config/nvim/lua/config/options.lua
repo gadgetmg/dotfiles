e87a558f02ef918d29b719c1d870b8175eae1f69
Matt Graham <gadgetmg@pm.me>
2024-12-23 20:47:30 -0500

    fix(nvim): Only set shellslash option on Windows

--- chezmoi/dot_config/nvim/lua/config/options.lua
+++ chezmoi/dot_config/nvim/lua/config/options.lua
@@ -1,8 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local windows = vim.fn.has("win32") == 1
 
 vim.g.snacks_animate = false
 
--- needed for orgmode on Windows
-vim.opt.shellslash = true
+if windows then
+  -- needed for orgmode on Windows
+  vim.opt.shellslash = true
+end
