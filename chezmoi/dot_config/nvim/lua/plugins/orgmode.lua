8c14807fa48b3643f71517c69cbaa29481765e35
Matt Graham <gadgetmg@pm.me>
2024-12-23 19:02:11 -0500

    feat(nvim): Add nvim-orgmode plugin

---
+++ chezmoi/dot_config/nvim/lua/plugins/orgmode.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    opts = {
+      org_agenda_files = "~/Documents/Notes/**/*",
+      org_default_notes_file = "~/Documents/Notes/refile.org",
+    },
+  },
+}
