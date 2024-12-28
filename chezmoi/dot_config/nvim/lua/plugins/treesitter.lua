50007e2b5664f198afed5437acb3db749b7d64e4
Matt Graham <gadgetmg@pm.me>
2024-12-28 13:56:04 -0500

    feat(nvim): Remap treesitter selection keymap to <CR>

---
+++ chezmoi/dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      incremental_selection = {
+        keymaps = {
+          init_selection = "<CR>",
+          node_incremental = "<CR>",
+        },
+      },
+    },
+  },
+}
