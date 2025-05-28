86cddec0097b633eace9986a745429eb8a88139f
Matt Graham <gadgetmg@pm.me>
2025-05-28 16:34:17 -0400

    fix(nixos): Update overlay for upstream mesa

--- overlays/upstream/default.nix
+++ overlays/upstream/default.nix
@@ -1,24 +1,13 @@
-{ channels, inputs, ... }:
+{
+  channels,
+  inputs,
+  lib,
+  ...
+}:
 
 final: prev: with final; {
   upstream = lib.makeScope newScope (_: {
     mesa =
-      let
-        extraRustDeps = [
-          {
-            pname = "rustc-hash";
-            version = "2.1.1";
-            hash = "sha256-3rQidUAExJ19STn7RtKNIpZrQUne2VVH7B1IO5UY91k=";
-          }
-        ];
-
-        copyRustDep = dep: ''
-          cp -R --no-preserve=mode,ownership ${fetchCrate dep} subprojects/${dep.pname}-${dep.version}
-          cp -R subprojects/packagefiles/${dep.pname}/* subprojects/${dep.pname}-${dep.version}/
-        '';
-
-        copyExtraRustDeps = lib.concatStringsSep "\n" (builtins.map copyRustDep extraRustDeps);
-      in
       (prev.mesa.override {
         directx-headers =
           # https://gitlab.freedesktop.org/mesa/mesa/-/issues/13126
@@ -38,10 +27,38 @@ final: prev: with final; {
             ./opencl.patch
           ];
           postPatch =
+            let
+              extraRustDeps = [
+                {
+                  pname = "rustc-hash";
+                  version = "2.1.1";
+                  hash = "sha256-3rQidUAExJ19STn7RtKNIpZrQUne2VVH7B1IO5UY91k=";
+                }
+              ];
+
+              copyRustDep = dep: ''
+                cp -R --no-preserve=mode,ownership ${fetchCrate dep} subprojects/${dep.pname}-${dep.version}
+                cp -R subprojects/packagefiles/${dep.pname}/* subprojects/${dep.pname}-${dep.version}/
+              '';
+
+              copyExtraRustDeps = lib.concatStringsSep "\n" (builtins.map copyRustDep extraRustDeps);
+            in
             prevAttrs.postPatch
             + ''
               ${copyExtraRustDeps}
             '';
+          mesonFlags =
+            let
+              unwantedFlags = [
+                "-Dgallium-nine=false"
+                "-Dgallium-xa=enabled"
+                "-Dosmesa=false"
+              ];
+            in
+            builtins.filter (flag: !(builtins.elem flag unwantedFlags)) prevAttrs.mesonFlags
+            ++ [
+              (lib.mesonEnable "gallium-mediafoundation" false)
+            ];
         });
   });
 }
