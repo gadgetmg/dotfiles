aa1c73897b6c8dbda1827261abc93e0b965a935a
Matt Graham <gadgetmg@pm.me>
2025-11-26 12:15:13 -0500

    feat(nvim): Install powershell LSP, switch to Telescope to avoid fzf Defender false positives

---
+++ chezmoi/dot_config/nvim/lua/plugins/powershell.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "TheLeoP/powershell.nvim",
+    ---@type powershell.user_config
+    opts = {
+      bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
+    },
+    ft = "ps1",
+  },
+}
