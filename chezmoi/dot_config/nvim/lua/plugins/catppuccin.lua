37b01f89efe6a5b62cb6ccb16bee95eec8447d2a
Matt Graham <gadgetmg@pm.me>
2026-01-04 10:28:03 -0500

    feat(nvim): Extend transparent background to floating windows

--- chezmoi/dot_config/nvim/lua/plugins/catppuccin.lua
+++ chezmoi/dot_config/nvim/lua/plugins/catppuccin.lua
@@ -3,6 +3,13 @@ return {
     "catppuccin",
     opts = {
       transparent_background = true,
+      custom_highlights = {
+        NormalFloat = { bg = "none" },
+        FloatBorder = { bg = "none" },
+        FloatTitle = { bg = "none" },
+        TelescopeBorder = { bg = "none" },
+        TelescopeTitle = { bg = "none" },
+      },
     },
   },
 }
