506fd3f4d0774e3d6ac3863d96801681213d299d
Matt Graham <gadgetmg@pm.me>
2026-02-07 23:41:56 -0500

    fix(nixos): Properly import flake-parts modules system

--- chezmoi/run_systemd__linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-systemctl --user add-wants default.target mpd.service
-systemctl --user add-wants mpd.service mpDris2.service
-systemctl --user add-wants sway-session.target waybar.service mako.service wallpaper.service foot-server.socket
