4b20917b8fdf9adf439b262e0e663f1e8bfb6b4e
Matt Graham <gadgetmg@pm.me>
2026-03-27 17:26:38 -0400

    feat(documents): Add metric-compatible fonts

--- modules/apps/documents.nix
+++ modules/apps/documents.nix
@@ -1,5 +1,7 @@
 {
   flake.modules.nixos.documents = {pkgs, ...}: {
+    fonts.packages = with pkgs; [liberation_ttf carlito caladea];
+
     environment.systemPackages = with pkgs; [
       libreoffice-qt-fresh
       zathura
