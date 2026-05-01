76561a634abe7acf9e5964177e87b00cf47d67c5
Matt Graham <gadgetmg@pm.me>
2026-05-01 11:07:01 -0400

    fix(nixos): Reduce snapshot interval

--- modules/snapshots.nix
+++ modules/snapshots.nix
@@ -5,14 +5,18 @@
     ...
   }: {
     services.snapper = {
-      snapshotInterval = "minutely";
+      snapshotInterval = "hourly";
       configs = let
         defaults = {
           FSTYPE = "btrfs";
           TIMELINE_CREATE = true;
           TIMELINE_CLEANUP = true;
-          TIMELINE_LIMIT_HOURLY = 48;
-          TIMELINE_LIMIT_DAILY = 7;
+          TIMELINE_LIMIT_HOURLY = 24;
+          TIMELINE_LIMIT_DAILY = 0;
+          TIMELINE_LIMIT_WEEKLY = 0;
+          TIMELINE_LIMIT_MONTHLY = 0;
+          TIMELINE_LIMIT_QUARTERLY = 0;
+          TIMELINE_LIMIT_YEARLY = 0;
         };
         mkIfEnabled = name: mount:
           lib.optionalAttrs (config.fileSystems.${mount}.enable or false)
