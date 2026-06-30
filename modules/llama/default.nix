93071e612cde9aecf194bee0ed1418393952692d
Matt Graham <gadgetmg@pm.me>
2026-06-30 12:25:56 -0400

    chore: lockfile maintenance

--- modules/llama/default.nix
+++ modules/llama/default.nix
@@ -46,7 +46,7 @@
         package = with pkgs;
           caddy.withPlugins {
             plugins = ["github.com/caddy-dns/cloudflare@v0.2.4"];
-            hash = "sha256-bzMqxWTqrJ1skZmRTXyEMCKStXpljbqe5r0Ve2cnBfM=";
+            hash = "sha256-8yZDrejNKsaUnUaTUFYbarWNmxafqp2z2rWo+XRsxV8=";
           };
         environmentFile = "/run/secrets/caddy.env";
         globalConfig = ''
